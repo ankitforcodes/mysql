35cc568f827d5531aa8c37db68366dad0c125bad
Ankit (Prabhatkumar) Sahay <ankitforcodes@gmail.com>
2023-03-25 15:33:15 +0530

    Create select_statements.sql

---
+++ head_first_sql/select_statements.sql
@@ -0,0 +1,12 @@
+-- Wildcard: %
+-- Allows for any number of characters wildcard matching
+select first_name from my_contact where first_name like "%m";
+
+-- above Query will result is "Tim", "Jim", "Slim", "Ibrahim" , etc
+
+
+-- Wildcard: _
+-- Allows for exact number of character wildcard matching
+select first_name from my_contact where first_name like "__m";
+
+-- above Query will result is "Tim", "Jim"
